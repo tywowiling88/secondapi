247765c84f3f9b70761da77a37fa2802d44f814f
Teofilus <teofilus.yohanes@student.umn.ac.id>
2021-06-21 21:54:12 +0700

    Read Single Data

---
+++ api/brand/read_single.php
@@ -0,0 +1,41 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '\xampp\htdocs\secondapi\config\Database.php';
+include_once '\xampp\htdocs\secondapi\models\Brand.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Create Object Brand 
+$brand = new Brand($db);
+
+// Get id_brand from URL 
+$brand->id_brand = isset($_GET['id_brand']) ? $_GET['id_brand'] : die();
+
+$result = $brand->ReadSingle();
+
+$row = $result->fetch(PDO::FETCH_ASSOC);
+
+// Set Properties 
+$brand->nama_brand = $row['nama_brand'];
+$brand->created_at = $row['created_at'];
+
+// Create an array and put the properties and value inside
+$brand_array = array(
+    'nama_brand' => $brand->nama_brand,
+    'created_at' => $brand->created_at
+);
+
+if ($brand->nama_brand != null && $brand->created_at != null) {
+    echo json_encode(
+        $brand_array
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Data Not Found')
+    );
+}
--- models/Brand.php
+++ models/Brand.php
@@ -6,8 +6,8 @@ class Brand
     private $table = 'brand';
 
     // Properties
-    public $id;
-    public $name;
+    public $id_brand;
+    public $nama_brand;
     public $created_at;
 
     // Constructor with DB
@@ -39,5 +39,29 @@ class Brand
     }
 
     // Read Single Data 
-    public function 
+    public function ReadSingle()
+    {
+        $query = 'SELECT 
+            id_brand,
+            nama_brand,
+            created_at
+        FROM 
+        ' . $this->table . '
+        WHERE 
+            id_brand = ?
+        ORDER BY
+            id_brand ASC
+        ';
+
+        // Prepare Query
+        $stmt = $this->conn->prepare($query);
+
+        // Bind Param
+        $stmt->bindParam(1, $this->id_brand);     //1 refer to first '?'
+
+        // Execute The query 
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
